b2411039101b1ea2841ec317bb7481be3f2e6914
Alex Linden <alextl2004@gmail.com>
2022-06-09 16:42:30 -0700

    created flash message and redirct logic

--- app.py
+++ app.py
@@ -7,42 +7,60 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
-responses = []
 
 
 @app.get("/")
 def show_survey():
-    """Homepage is rendered with with start information about survey and start button"""
-
+    """Homepage is rendered with with start information about survey and start button,
+    initiates session"""
+    session["responses"] = []
     return render_template("survey_start.html", survey=survey)
 
 
 @app.post('/begin')
-def start_survay():
+def start_survey():
     """takes in a post request to clear responses global variable
     and redircts to questions page"""
 
-    responses.clear()
-    id = len(responses)
-    return redirect(f"/questions/{id}")
+    q_id = 0
+    return redirect(f"/questions/{q_id}")
 
 
-@app.get("/questions/<int:id>")
-def ask_question(id):
+@app.get("/questions/<int:q_id>")
+def ask_question(q_id):
     """takes in an ID and updates question route to reflect corresponding question
-    and returns that page"""
+    and returns that page, checks if user is trying to access wrong question route and
+    redircts them if they are with a flash message"""
 
-    return render_template("question.html", question=survey.questions[id])
+    responses = session['responses']
+    if len(responses) == len(survey.questions):
+        flash("you've already answered all the question")
+        return redirect('/complete')
+    elif not q_id == len(responses):
+        q_id = len(responses)
+        flash("please answer the questions in order")
+        return redirect(f"/questions/{q_id}")
+
+    return render_template("question.html", question=survey.questions[q_id])
 
 
 @app.post('/answer')
 def get_answer():
-    """stores submited answer and updates question route and redircts to question request
-    or shows completion page when all questions are answered"""
+    """stores submited answer in a session and updates question route and
+    redircts to question request or redirects to completion page
+    when all questions are answered"""
 
+    responses = session['responses']
     responses.append(request.form['answer'])
-    id = len(responses)
-    if id < len(survey.questions):
-        return redirect(f"/questions/{id}")
+    session['responses'] = responses
+    q_id = len(responses)
+    if q_id < len(survey.questions):
+        return redirect(f"/questions/{q_id}")
     else:
-        return render_template('completion.html')
+        return redirect('/complete')
+
+@app.get('/complete')
+def get_complete_stage():
+    """display completion page"""
+    print(session['responses'])
+    return render_template('completion.html')
