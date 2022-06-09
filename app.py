da97f2c9cbd2e7b03685581149bc322224a0a72c
LaithLou <laithlouay@gmail.com>
2022-06-09 17:58:14 -0400

    created iterable question pages and routes to each page

--- app.py
+++ app.py
@@ -7,3 +7,32 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+responses = []
+
+
+@app.get("/")
+def show_survey():
+    return render_template("survey_start.html", survey=survey)
+
+
+@app.post('/begin')
+def start_survay():
+
+    return redirect('/questions/0')
+
+
+@app.get("/questions/<int:id>")
+def ask_question(id):
+
+    return render_template("question.html", question=survey.questions[id])
+
+
+@app.post('/answer')
+def get_answer():
+
+    responses.append(request.form['answer'])
+    id = len(responses)
+    if id < len(survey.questions):
+        return redirect(f"/questions/{id}")
+    else:
+        return render_template('completion.html')
